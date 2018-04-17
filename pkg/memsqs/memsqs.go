4c34314617f731568bd2baccfe49451bb18bc253
Ben Marini <ben@remind101.com>
2018-04-17 13:51:30 -0700

    Add more tests to memsqs

--- pkg/memsqs/memsqs.go
+++ pkg/memsqs/memsqs.go
@@ -1,6 +1,7 @@
 package memsqs
 
 import (
+	"strconv"
 	"sync"
 	"time"
 
@@ -10,7 +11,8 @@ import (
 	"github.com/google/uuid"
 )
 
-const DefaultVisibilityTimeout = 30 * time.Second
+// DefaultVisibilityTimeout is the SQS default.
+const DefaultVisibilityTimeout time.Duration = 30 * time.Second
 
 // Client is an in-memory implementation of sqsiface.SQSAPI.
 // It is NOT a complete implementation yet. Functionality will
@@ -18,47 +20,70 @@ const DefaultVisibilityTimeout = 30 * time.Second
 type Client struct {
 	sync.Mutex
 	sqsiface.SQSAPI
-	queues map[string][]*message
+	queues map[string][]*Message
 }
 
-type message struct {
-	message      *sqs.Message
-	receivedTime time.Time
-	visibleAfter time.Time
+// Message wraps an sqs.Message with metadata.
+type Message struct {
+	SQSMessage   *sqs.Message
+	ReceivedTime time.Time
+	VisibleAfter time.Time
 }
 
+// New returns a new memsqs.Client.
 func New() *Client {
 	return &Client{
-		queues: map[string][]*message{},
+		queues: map[string][]*Message{},
 	}
 }
 
-func (c *Client) Queue(name string) []*message {
+// VisibleQueue returns only items that are currently visible.
+func (c *Client) VisibleQueue(name string) []*Message {
+	rq := []*Message{}
+	if q, ok := c.queues[name]; ok {
+		now := time.Now()
+		for _, m := range q {
+			if m.VisibleAfter.Before(now) {
+				rq = append(rq, m)
+			}
+		}
+	}
+	return rq
+}
+
+// Queue provides direct access to a queue of messages.
+func (c *Client) Queue(name string) []*Message {
 	if q, ok := c.queues[name]; ok {
 		return q
 	}
-	return []*message{}
+	return []*Message{}
 }
 
+// SendMessage satisfies the sqsiface.SQSAPI interface.
 func (c *Client) SendMessage(params *sqs.SendMessageInput) (*sqs.SendMessageOutput, error) {
 	c.Lock()
 	defer c.Unlock()
 
-	msg := &message{
-		message: &sqs.Message{
+	if params.MessageAttributes == nil {
+		params.MessageAttributes = map[string]*sqs.MessageAttributeValue{}
+	}
+
+	msg := &Message{
+		SQSMessage: &sqs.Message{
 			Body:              params.MessageBody,
 			MessageAttributes: params.MessageAttributes,
 			ReceiptHandle:     aws.String(uuid.New().String()),
 		},
-		visibleAfter: time.Now(),
+		VisibleAfter: time.Now(),
 	}
 
-	msg.visibleAfter = msg.visibleAfter.Add(time.Duration(aws.Int64Value(params.DelaySeconds)) * time.Second)
+	msg.VisibleAfter = msg.VisibleAfter.Add(time.Duration(aws.Int64Value(params.DelaySeconds)) * time.Second)
 	c.queues[aws.StringValue(params.QueueUrl)] = append(c.queues[aws.StringValue(params.QueueUrl)], msg)
 
 	return &sqs.SendMessageOutput{}, nil
 }
 
+// ReceiveMessage satisfies the sqsiface.SQSAPI interface.
 func (c *Client) ReceiveMessage(params *sqs.ReceiveMessageInput) (*sqs.ReceiveMessageOutput, error) {
 	c.Lock()
 	defer c.Unlock()
@@ -67,27 +92,23 @@ func (c *Client) ReceiveMessage(params *sqs.ReceiveMessageInput) (*sqs.ReceiveMe
 		Messages: []*sqs.Message{},
 	}
 
-	q := c.Queue(aws.StringValue(params.QueueUrl))
+	q := c.VisibleQueue(aws.StringValue(params.QueueUrl))
 
 	max := int(aws.Int64Value(params.MaxNumberOfMessages))
 	if len(q) < max {
 		max = len(q)
 	}
 
-	now := time.Now()
 	vt := DefaultVisibilityTimeout
 	if params.VisibilityTimeout != nil {
 		vt = time.Duration(aws.Int64Value(params.VisibilityTimeout)) * time.Second
 	}
 
 	for _, m := range q {
-		if m.visibleAfter.Before(now) {
-			if m.receivedTime.IsZero() {
-				m.receivedTime = now
-			}
-			data.Messages = append(data.Messages, m.message)
-			m.visibleAfter = m.receivedTime.Add(vt)
-		}
+		data.Messages = append(data.Messages, m.SQSMessage)
+		m.VisibleAfter = m.ReceivedTime.Add(vt)
+		setMessageReceived(m, vt)
+
 		if len(data.Messages) >= max {
 			return data, nil
 		}
@@ -96,6 +117,30 @@ func (c *Client) ReceiveMessage(params *sqs.ReceiveMessageInput) (*sqs.ReceiveMe
 	return data, nil
 }
 
+func setMessageReceived(m *Message, vt time.Duration) {
+	now := time.Now()
+	// Set ReceivedTime
+	if m.ReceivedTime.IsZero() {
+		m.ReceivedTime = now
+	}
+
+	// Set VisibilityTimeout
+	m.VisibleAfter = m.ReceivedTime.Add(vt)
+
+	// Set ApproximateReceiveCount
+	rc := 0
+	if v, ok := m.SQSMessage.MessageAttributes[sqs.MessageSystemAttributeNameApproximateReceiveCount]; ok {
+		rc, _ = strconv.Atoi(aws.StringValue(v.StringValue))
+	}
+	rc++
+	rcStringValue := strconv.Itoa(rc)
+	m.SQSMessage.MessageAttributes[sqs.MessageSystemAttributeNameApproximateReceiveCount] = &sqs.MessageAttributeValue{
+		DataType:    aws.String("Number"),
+		StringValue: aws.String(rcStringValue),
+	}
+}
+
+// ChangeMessageVisibility satisfies the sqsiface.SQSAPI interface.
 func (c *Client) ChangeMessageVisibility(params *sqs.ChangeMessageVisibilityInput) (*sqs.ChangeMessageVisibilityOutput, error) {
 	c.Lock()
 	defer c.Unlock()
@@ -105,8 +150,8 @@ func (c *Client) ChangeMessageVisibility(params *sqs.ChangeMessageVisibilityInpu
 
 	q := c.Queue(aws.StringValue(params.QueueUrl))
 	for _, m := range q {
-		if aws.StringValue(m.message.ReceiptHandle) == aws.StringValue(params.ReceiptHandle) {
-			m.visibleAfter = m.receivedTime.Add(vt)
+		if aws.StringValue(m.SQSMessage.ReceiptHandle) == aws.StringValue(params.ReceiptHandle) {
+			m.VisibleAfter = m.ReceivedTime.Add(vt)
 			break
 		}
 	}
@@ -114,6 +159,7 @@ func (c *Client) ChangeMessageVisibility(params *sqs.ChangeMessageVisibilityInpu
 	return data, nil
 }
 
+// DeleteMessage satisfies the sqsiface.SQSAPI interface.
 func (c *Client) DeleteMessage(params *sqs.DeleteMessageInput) (*sqs.DeleteMessageOutput, error) {
 	c.Lock()
 	defer c.Unlock()
@@ -121,7 +167,7 @@ func (c *Client) DeleteMessage(params *sqs.DeleteMessageInput) (*sqs.DeleteMessa
 
 	q := c.Queue(aws.StringValue(params.QueueUrl))
 	for i, m := range q {
-		if aws.StringValue(m.message.ReceiptHandle) == aws.StringValue(params.ReceiptHandle) {
+		if aws.StringValue(m.SQSMessage.ReceiptHandle) == aws.StringValue(params.ReceiptHandle) {
 			c.queues[aws.StringValue(params.QueueUrl)] = append(q[:i], q[i+1:]...)
 			break
 		}
--- pkg/memsqs/memsqs_test.go
+++ pkg/memsqs/memsqs_test.go
@@ -2,10 +2,12 @@ package memsqs_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/remind101/mq-go/pkg/memsqs"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -19,8 +21,70 @@ func TestSendMessage(t *testing.T) {
 				StringValue: aws.String("/jobs/priority"),
 			},
 		},
-		MessageBody: aws.String(""),
+		MessageBody: aws.String("hello"),
+	})
+	require.NoError(t, err)
+	require.Equal(t, 1, len(c.Queue("jobs")))
+	assert.Equal(t, "hello", aws.StringValue(c.Queue("jobs")[0].SQSMessage.Body))
+}
+
+func TestReceiveMessage(t *testing.T) {
+	c := memsqs.New()
+
+	// Send a message.
+	_, err := c.SendMessage(&sqs.SendMessageInput{
+		QueueUrl: aws.String("jobs"),
+		MessageAttributes: map[string]*sqs.MessageAttributeValue{
+			"group": &sqs.MessageAttributeValue{
+				DataType:    aws.String("String"),
+				StringValue: aws.String("/jobs/priority"),
+			},
+		},
+		MessageBody: aws.String("job1"),
 	})
 	require.NoError(t, err)
 
+	// Receive message.
+	out, err := c.ReceiveMessage(&sqs.ReceiveMessageInput{
+		MaxNumberOfMessages: aws.Int64(1),
+		QueueUrl:            aws.String("jobs"),
+		VisibilityTimeout:   aws.Int64(1),
+	})
+
+	// Assert message no longer visible.
+	require.Equal(t, 1, len(out.Messages))
+	require.Equal(t, 1, len(c.Queue("jobs")))
+	require.Equal(t, 0, len(c.VisibleQueue("jobs")))
+
+	assert.Equal(t, "job1", aws.StringValue(out.Messages[0].Body))
+	receiveCount, ok := out.Messages[0].MessageAttributes[sqs.MessageSystemAttributeNameApproximateReceiveCount]
+	require.True(t, ok)
+	assert.Equal(t, "1", aws.StringValue(receiveCount.StringValue))
+
+	time.Sleep(1 * time.Second)
+	time.Sleep(1 * time.Millisecond)
+
+	// Assert message is visible again.
+	require.Equal(t, 1, len(c.VisibleQueue("jobs")))
+
+	// Assert receive count increments.
+	out, err = c.ReceiveMessage(&sqs.ReceiveMessageInput{
+		MaxNumberOfMessages: aws.Int64(1),
+		QueueUrl:            aws.String("jobs"),
+		VisibilityTimeout:   aws.Int64(1),
+	})
+	receiveCount, ok = out.Messages[0].MessageAttributes[sqs.MessageSystemAttributeNameApproximateReceiveCount]
+	require.True(t, ok)
+	assert.Equal(t, "2", aws.StringValue(receiveCount.StringValue))
+
+	// Delete message.
+	_, err = c.DeleteMessage(&sqs.DeleteMessageInput{
+		QueueUrl:      aws.String("jobs"),
+		ReceiptHandle: out.Messages[0].ReceiptHandle,
+	})
+	require.NoError(t, err)
+
+	// Assert message no longer exists.
+	require.Equal(t, 0, len(c.VisibleQueue("jobs")))
+	require.Equal(t, 0, len(c.Queue("jobs")))
 }
