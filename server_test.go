484895dc4753e78619e072968f6399f728b71c7a
Ben Marini <ben@remind101.com>
2018-05-01 14:34:11 -0700

    Add another test

--- server_test.go
+++ server_test.go
@@ -3,6 +3,7 @@ package mq_test
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/sqs"
@@ -12,9 +13,14 @@ import (
 )
 
 func TestServer(t *testing.T) {
-	done := make(chan struct{})
 	qURL := "jobs"
+	done := make(chan struct{})
 	c := memsqs.New()
+
+	c.SendMessage(&sqs.SendMessageInput{
+		QueueUrl:    aws.String(qURL),
+		MessageBody: aws.String(`{"name":"test"}`),
+	})
 
 	h := mq.HandlerFunc(func(m *mq.Message) error {
 		assert.Equal(t, `{"name":"test"}`, aws.StringValue(m.SQSMessage.Body))
@@ -30,12 +36,43 @@ func TestServer(t *testing.T) {
 	})
 
 	sp.Start()
-	defer sp.Shutdown(context.Background())
+	<-done             // Wait for server to handle message
+	closeServer(t, sp) // Wait for server to shutdown
+	assert.Empty(t, len(c.Queue(qURL)))
+}
+
+func TestServerWaitsForProcessingToFinish(t *testing.T) {
+	qURL := "jobs"
+	done := make(chan struct{})
+	c := memsqs.New()
 
 	c.SendMessage(&sqs.SendMessageInput{
 		QueueUrl:    aws.String(qURL),
 		MessageBody: aws.String(`{"name":"test"}`),
 	})
 
-	<-done
+	h := mq.HandlerFunc(func(m *mq.Message) error {
+		close(done)
+		time.Sleep(10 * time.Millisecond)
+		return nil
+	})
+
+	sp := mq.NewServer(qURL, h, func(s *mq.Server) {
+		s.Client = c
+		s.ErrorHandler = func(err error) {
+			t.Fatal(err)
+		}
+	})
+
+	sp.Start()
+	<-done             // Wait for server to receive message
+	closeServer(t, sp) // Wait for server to shutdown
+	assert.Empty(t, len(c.Queue(qURL)))
+}
+
+func closeServer(t *testing.T, s *mq.Server) {
+	t.Helper()
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+	assert.NoError(t, s.Shutdown(ctx))
 }
